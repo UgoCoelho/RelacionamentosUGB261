43b3f8cf9f9ae792c88da351d0967ff692fd74e0
UGO COELHO <2024102070@academicougb.com.br>
2026-05-26 21:30:04 -0300

    Adicionar arquivos de projeto.

---
+++ Relacionamentos/Models/Blog.cs
@@ -0,0 +1,11 @@
+﻿namespace Relacionamentos.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string? Name { get; set; }
+        public virtual Uri? SiteUri { get; set; }
+
+        public ICollection<Post>? Posts { get;}
+    }
+}
